c767b8a2c00b52f40beb39334dc830a36cf580e4
Bahyu Sanciko <41245088+bahyusanciko@users.noreply.github.com>
2023-03-10 20:48:54 +0700

    trim all key and value

--- jsonToInterface.ts
+++ jsonToInterface.ts
@@ -5,19 +5,19 @@ export function jsonToInterface(jsonObj: any, interfaceName?: string) {
 
   for (const [key, value] of Object.entries(jsonObj)) {
     if (value === null) {
-      interfaceStr += `${key}: null;\n`;
+      interfaceStr += `${key.trim()}: null;\n`;
     } else if (typeof value === "object" && !Array.isArray(value)) {
-      interfaceStr += `${key}: ${jsonToInterface(value)}`;
+      interfaceStr += `${key.trim()}: ${jsonToInterface(value)}`;
     } else if (Array.isArray(value)) {
       let arrayType: string;
-      if (value.length === 0 || typeof value[0] !== "object") {
-        arrayType = `${typeof value[0]}[]`;
+      if (value.length === 0 || typeof value[0].trim() !== "object") {
+        arrayType = `${typeof value[0].trim()}[]`;
       } else {
-        arrayType = `Array<${jsonToInterface(value[0])}>`;
+        arrayType = `Array<${jsonToInterface(value[0].trim())}>`;
       }
-      interfaceStr += `${key}: ${arrayType};\n`;
+      interfaceStr += `${key.trim()}: ${arrayType};\n`;
     } else {
-      interfaceStr += `${key}: ${typeof value};\n`;
+      interfaceStr += `${key.trim()}: ${typeof value.trim()};\n`;
     }
   }
 
